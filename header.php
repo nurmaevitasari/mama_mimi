ed6f1df81d90f315a7f68341fe24382fdd6ef09b
Nurma Evitasari <evitasarinurma@gmail.com>
2022-06-18 10:54:35 +0700

    Update Header

--- header.php
+++ header.php
@@ -13,7 +13,7 @@ session_start();
     <meta name="author" content="">
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
-    <title>Hexashop Ecommerce HTML CSS Template</title>
+    <title>Mama Mimi</title>
 
 
     <!-- Additional CSS Files -->
@@ -26,13 +26,6 @@ session_start();
     <link rel="stylesheet" href="assets/css/owl-carousel.css">
 
     <link rel="stylesheet" href="assets/css/lightbox.css">
-<!--
-
-TemplateMo 571 Hexashop
-
-https://templatemo.com/tm-571-hexashop
-
--->
     </head>
     
     <body>
